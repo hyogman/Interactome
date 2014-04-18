35893c0c7795608e7e77a4b48eb5ae04427976f7
henry <yogmanh@seattleu.edu>
2014-04-18 15:41:43 -0700

    added the recsys files

---
+++ recsys/recsys/views.py
@@ -0,0 +1,17 @@
+#from django.shortcuts import render
+import json
+from django.core.context_processors import csrf
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+import boto 
+
+
+
+# Create your views here.
+@csrf_exempt
+def getRecs(request):
+	print request.body
+	return HttpResponse('Success')
+
+
+
---
+++ recsys/views.py
@@ -0,0 +1,12 @@
+#from django.shortcuts import render
+#import boto 
+from django.http import HttpResponse
+
+
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("Hello world")
+
+
+
