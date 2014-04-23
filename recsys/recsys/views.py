82d5075c243ea2f8b8d6bb4f5789f2f10b945303
henry <henryyogman@gmail.com>
2014-04-23 15:30:17 -0700

    added recsys

--- recsys/recsys/views.py
+++ recsys/recsys/views.py
@@ -3,36 +3,53 @@ import json
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from boto import dynamodb2
+import boto.dynamodb
 import sys
+import csv
+import logging
+import argparse
+import time
 
 
+
+SECRET_KEY_EXCEL_FIELD = 'Secret Access Key'
+ACCESS_KEY_EXCEL_FIELD = 'Access Key Id'
+
+
+
+
+# Create your views here.
+
+#this function is essentially the main it maps to the url and calls 
+# other functions from here
+@csrf_exempt
+def getRecs(request):
+	limit = 0
+	numberOfAbstracts = 0 
+	print request.body
+	if request.method == 'POST':
+		numberofAbstracts = numberOfAbstracts + 1
+		abstractList = request.body
+		limit = 100 + numberOfAbstracts
+		if (numberofAbstracts > 0):
+			connectAWS()
+			print numberOfAbstracts
+	return HttpResponse("success")
+	#limit = request.size
 
 
 def connectAWS():
 	REGION = 'us-west-2'
 	try:
-		dynamoConn = dynamodb2.connect_to_region(REGION, aws_access_key_id=accessKey, aws_secret_access_key=secretKey)
+		csvfile = open('/Users/henryyogman/Desktop/credentials.csv', 'rU')
+		reader = csv.DictReader(csvfile) 
+		authLine = reader.next()
+		accessKey = authLine[ACCESS_KEY_EXCEL_FIELD]
+        secretKey = authLine[SECRET_KEY_EXCEL_FIELD]
+        dynamoConn = dynamodb2.connect_to_region(REGION, aws_access_key_id=accessKey, aws_secret_access_key=secretKey)
 	except Exception, e:
 		print(e)
 	sys.exit(1)
-
-# Create your views here.
-@csrf_exempt
-def getRecs(request):
-	limit = 0
-	numberofAbstracts = 0 
-	print request.body
-	if request.method == 'POST':
-		numberofAbstracts = numberofAbstracts + 1
-		abstractList = request.body
-		limit = 100 + numberofAbstracts
-		if (numberofAbstracts > 0):
-			#connectAWS()
-			print numberofAbstracts
-	return HttpResponse("success")
-	#limit = request.size
-
 
 
 
