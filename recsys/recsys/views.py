f7ddad92ee4b3c8f1120ef66e0c8fff39af411b4
henry <henryyogman@gmail.com>
2014-04-24 14:11:49 -0700

    So got AWS connected, need to figure out how to determine number of abstracts on django side

--- recsys/recsys/views.py
+++ recsys/recsys/views.py
@@ -3,53 +3,50 @@ import json
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-import boto.dynamodb
+#import boto.dynamodb
+from boto import dynamodb2
 import sys
 import csv
 import logging
 import argparse
 import time
-
-
+from boto.s3.key import Key
+from boto.dynamodb2.table import Table
+from boto import s3
 
 SECRET_KEY_EXCEL_FIELD = 'Secret Access Key'
 ACCESS_KEY_EXCEL_FIELD = 'Access Key Id'
 
-
-
-
-# Create your views here.
-
-#this function is essentially the main it maps to the url and calls 
-# other functions from here
+#this function is essentially the main it maps to the url /recs/ and 
+# for some reason I cannot call other functions out of here, it needs to be mapped to new URL
 @csrf_exempt
 def getRecs(request):
 	limit = 0
 	numberOfAbstracts = 0 
 	print request.body
 	if request.method == 'POST':
-		numberofAbstracts = numberOfAbstracts + 1
+		numberOfAbstracts = numberOfAbstracts + 1
 		abstractList = request.body
 		limit = 100 + numberOfAbstracts
-		if (numberofAbstracts > 0):
-			#connectAWS()
+		if (numberOfAbstracts > 0):
+		# does authentication for AWS, will not need this when EC2 is running. 
+			REGION = 'us-west-2'
+			try:
+				csvfile = open('/Users/henryyogman/Desktop/credentials.csv', 'rU')
+				reader = csv.DictReader(csvfile) 
+				authLine = reader.next()	
+				accessKey = authLine[ACCESS_KEY_EXCEL_FIELD]
+				secretKey = authLine[SECRET_KEY_EXCEL_FIELD]
+				dynamoConn = dynamodb2.connect_to_region(REGION, aws_access_key_id=accessKey, aws_secret_access_key=secretKey)
+			except Exception, e:
+				print "failed to connect!"
+				print(e)
+				sys.exit(1)
 			print numberOfAbstracts
 	return HttpResponse("success")
 	#limit = request.size
 
 
-def connectAWS():
-	REGION = 'us-west-2'
-	try:
-		csvfile = open('/Users/henryyogman/Desktop/credentials.csv', 'rU')
-		reader = csv.DictReader(csvfile) 
-		authLine = reader.next()
-		accessKey = authLine[ACCESS_KEY_EXCEL_FIELD]
-        #secretKey = authLine[SECRET_KEY_EXCEL_FIELD]
-        #dynamoConn = dynamodb2.connect_to_region(REGION, aws_access_key_id=accessKey, aws_secret_access_key=secretKey)
-	except Exception, e:
-		print(e)
-	sys.exit(1)
 
 
 
