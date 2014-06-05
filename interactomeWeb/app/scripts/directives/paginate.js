10cef0c5903d2c5a8c361fd642fa03d9ab3a55b7
Nathan Lucyk <nlucyk@cellnetix.com>
2014-06-05 02:09:49 -0700

    paginate not showing when using cache and animation

--- interactomeWeb/app/scripts/directives/paginate.js
+++ interactomeWeb/app/scripts/directives/paginate.js
@@ -12,16 +12,26 @@ angular.module('interactomeApp')
         filtered: '=',
         nonFiltered: '='
       },
-      controller: ['$scope', function($scope) {
+      controller: ['$scope', '$timeout', function($scope, $timeout) {
         $scope.paginate = function() {
           // from http://stackoverflow.com/questions/18445590/jquery-animate-stop-scrolling-when-user-scrolls-manually
-          // I'm so sorry - Nathan on the eve of projects day
+          // sorry - Nathan on the eve of projects day
+          // This was added to avoid a jarring effect when trying to scroll at the start of the page.
+          // the timeout is needed to allow an animation in the future to occur without allowing 
+          // the 7 or so animation calls that occur... a better solution should be attempted in the future.
           $("body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
-             $('body').stop();
-         });
-         $('body').animate({scrollTop: 0}, 'normal', function(){
              $("body").unbind("scroll mousedown DOMMouseScroll mousewheel keyup");
-         });
+             $('body').stop();
+             $timeout(function(){$scope.animateAllowed=true;}, 400, true);
+          });
+
+          if($scope.animateAllowed) {
+            $scope.animateAllowed = false;
+            $('body').animate({scrollTop: 0}, 'normal', function(){
+                $scope.animateAllowed = true;
+                 $("body").unbind("scroll mousedown DOMMouseScroll mousewheel keyup");
+             });
+          }
           // Setting page to 0 is a hack to get the recs working on page 1.
           if ($scope.page == 0)
               $scope.page = 1;
